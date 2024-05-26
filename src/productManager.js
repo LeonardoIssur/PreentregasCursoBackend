fab4d2cc81a71afba8a05c6ff33d8b3da3363e16
Issur Pina <leonardoissur.2804@gmail.com>
2024-05-26 17:15:26 -0500

    segundaPreentrega

--- src/productManager.js
+++ src/productManager.js
@@ -1,69 +1,61 @@
-import { promises as fs } from 'fs'
-import { v4 as uuidv4 } from 'uuid'
+import { promises as fs } from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
-export class ProductManager {
-    constructor(){
+export default class ProductManager {
+    constructor() {
         this.path = 'products.json';
-        this.products = []
+        this.products = [];
     }
 
-    addProduct = async ({ title, description, price, thumbnail, code, stock, status, category }) => {
-        const id = uuidv4()
+    addProduct = async ({ title, description, price, thumbnail, code, stock, status = true, category }) => {
+        const id = uuidv4();
 
-        let newProduct = { id, title, description, price, thumbnail, code, stock, status, category}
-        
-        this.products = await this.getProducts()
-        this.products.push(newProduct)
+        const newProduct = { id, title, description, price, thumbnail, code, stock, status, category };
 
-        awaitfs.writeFile(this.path, JSON.stringify(this.products))
+        this.products = await this.getProducts();
+        this.products.push(newProduct);
+
+        await fs.writeFile(this.path, JSON.stringify(this.products, null, 2));
 
         return newProduct;
     }
- 
+
     getProducts = async () => {
-        const response = await fs.readFile(this.path, 'utf8')
-        const responseJSON = JSON.parse(response)
-
-        return responseJSON; 
-    }
-
-    getProductsById = async (id) => {
-        const response =  await this.getProducts()
-
-        const product = response.find(product => product.id === id )
-
-        if(product){
-            return product
-        } else {
-            console.log('Producto no encontrado')
+        try {
+            const response = await fs.readFile(this.path, 'utf8');
+            return JSON.parse(response);
+        } catch (error) {
+            return [];
         }
     }
 
-    updateProduct = async (id, {...data}) => {
-        const products =  await this.getProducts()
-        const index = response.findindex(product => product.id === id)
+    getProductById = async (id) => {
+        const products = await this.getProducts();
+        return products.find(product => product.id === id) || null;
+    }
 
-        if(index !== -1 ){
-            products[index] = {id, ...data}
-            await fs.writeFile(this.path, JSON.stringify(this.products))
-            return this.products[index]
+    updateProduct = async (id, data) => {
+        const products = await this.getProducts();
+        const index = products.findIndex(product => product.id === id);
+
+        if (index !== -1) {
+            products[index] = { ...products[index], ...data };
+            await fs.writeFile(this.path, JSON.stringify(products, null, 2));
+            return products[index];
         } else {
-            console.log('producto no encontrado');
+            return null;
         }
     }
 
-    delateProduct = async (id) => {
-        const products =  await this.getProducts()
-        const index = response.findindex(product => product.id === id)
+    deleteProduct = async (id) => {
+        const products = await this.getProducts();
+        const index = products.findIndex(product => product.id === id);
 
-        if(index !== -1){
-            products.splice(index, 1)
-            await fs.writeFile(this.path, JSON.stringify(products))
+        if (index !== -1) {
+            products.splice(index, 1);
+            await fs.writeFile(this.path, JSON.stringify(products, null, 2));
         } else {
-            console.log('producto no encontrado')
+            console.log('Producto no encontrado');
         }
     }
-
 }
-
-export default ProductManager;
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { productManager } from "../index,js";
+import { productManager } from "../../index.js";
 
 const productsRouter = Router()
 
